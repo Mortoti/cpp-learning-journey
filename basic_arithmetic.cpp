93bc9d2e9ccb0855dfaba9a0c714945cf23e5d2b
Mortoti-Agogyi Jephthah Lorlornyo <mortoti.dev@gmail.com>
2025-11-07 11:33:04 +0000

    Added C++ program performing basic arithmetic operations step by step

---
+++ basic_arithmetic.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    system("color 3f");
+    int result = 1 + 2;
+    cout << result << endl;
+    result = result - 1;
+    cout<< result <<endl;
+    result = result * 2;
+    cout <<result<<endl;
+    result = result / 2;
+    cout <<result <<endl;
+    result = result * 8;
+    cout<<result<<endl;
+    result = result % 7;
+    cout << result <<endl;
+    system("pause");
+    return 0;
+}
