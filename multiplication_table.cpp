31bccd993ff15779c59d679a556e64fa9e4daad0
Mortoti-Agogyi Jephthah Lorlornyo <mortoti.dev@gmail.com>
2025-11-07 11:08:18 +0000

    Added C++ program to display the 2 times multiplication table using a for loop

---
+++ multiplication_table.cpp
@@ -0,0 +1,14 @@
+// 2 times multiplication table
+#include <iostream>
+using namespace std;
+int main ()
+{
+    cout << "Multiplication Table" << endl;
+
+    for ( int i= 1; i <= 12; i++){
+        cout<< " 2 * "<< i << " = " <<2 * i<< endl;
+
+    }
+    return 0;
+}
+
